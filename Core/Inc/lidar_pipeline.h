0c512be82c41265cf9c8378d042ae0dbc739cda1
fuzi233 <fuzi1fuz1@163.com>
2026-05-22 18:15:09 +0800

    更新激光雷达处理流程

--- Core/Inc/lidar_pipeline.h
+++ Core/Inc/lidar_pipeline.h
@@ -10,16 +10,13 @@ extern "C" {
 
 #include "rplidar.h"
 
-/* 简化的MotorSyncSnapshot - 用于兼容旧代码 */
-typedef struct {
-  int32_t left_encoder;
-  int32_t right_encoder;
-  float left_speed_mm_s;
-  float right_speed_mm_s;
-  bool manual_mode;
-  uint32_t fault_flags;
-  uint32_t control_timestamp_ms;
-} MotorSyncSnapshot;
+#define LIDAR_ANALYSIS_PERIOD_MS         50U
+#define LIDAR_RAW_FRAME_CAPACITY         256U
+#define LIDAR_SECTOR_COUNT               32U
+#define LIDAR_LOCAL_GRID_WIDTH           64U
+#define LIDAR_LOCAL_GRID_HEIGHT          64U
+#define LIDAR_LOCAL_GRID_RESOLUTION_MM   50U
+#define LIDAR_LOCAL_GRID_ORIGIN_VEHICLE  0U
 
 typedef void (*LidarPipelineDebugPrint)(const char *message);
 
@@ -33,31 +30,61 @@ typedef struct
 
 typedef struct
 {
-  uint32_t block_id;
-  uint16_t offset;
-  uint16_t length;
-  uint32_t dma_timestamp_ms;
-  uint32_t sample_timestamp_ms;
-  uint32_t valid_sample_count;
-  uint32_t parse_error_count;
-  bool has_sample;
-  RPLidarScanSample latest_sample;
-  float min_distance_mm;
-  float filtered_min_distance_mm;
-  uint32_t total_nodes_extracted;
-  bool sector_obstacle[8];
-  float sector_min_distance[8];
-  bool filtered_sector_obstacle[8];
-  float filtered_sector_distance[8];
-  uint8_t filter_sample_count[8];
-  uint32_t sync_delta_ms;
-  MotorSyncSnapshot sync_snapshot;
+  uint32_t frame_id;
+  uint32_t timestamp_ms;
+  uint16_t sample_count;
+  LidarRawNode samples[LIDAR_RAW_FRAME_CAPACITY];
+} LidarRawFrame;
+
+typedef struct
+{
+  bool valid;
+  uint16_t hit_count;
+  uint16_t min_distance_mm;
+  uint16_t mean_distance_mm;
+  uint32_t stale_ms;
+} LidarSector32Entry;
+
+typedef struct
+{
+  uint32_t frame_id;
+  uint32_t timestamp_ms;
+  uint8_t sector_count;
+  LidarSector32Entry sectors[LIDAR_SECTOR_COUNT];
+} LidarSector32Frame;
+
+typedef enum
+{
+  LIDAR_GRID_CELL_UNKNOWN = 0,
+  LIDAR_GRID_CELL_FREE = 1,
+  LIDAR_GRID_CELL_OCCUPIED = 2
+} LidarLocalGridCellState;
+
+typedef struct
+{
+  uint32_t frame_id;
+  uint32_t timestamp_ms;
+  uint8_t width;
+  uint8_t height;
+  uint16_t resolution_mm;
+  uint8_t origin_mode;
+  uint8_t cells[LIDAR_LOCAL_GRID_HEIGHT][LIDAR_LOCAL_GRID_WIDTH];
+} LidarLocalGridFrame;
+
+typedef struct
+{
+  LidarRawFrame raw_frame;
+  LidarSector32Frame sector_frame;
+  LidarLocalGridFrame grid_frame;
 } LidarParseResult;
 
 bool LidarPipeline_Start(RPLidarContext *ctx, LidarPipelineDebugPrint debug_print);
 void LidarPipeline_UartRxEventFromISR(uint16_t size);
 void LidarPipeline_DmaHalfCompleteFromISR(void);
 void LidarPipeline_DmaCompleteFromISR(void);
+bool LidarPipeline_GetLatestRawFrame(LidarRawFrame *out);
+bool LidarPipeline_GetLatestSector32Frame(LidarSector32Frame *out);
+bool LidarPipeline_GetLatestLocalGridFrame(LidarLocalGridFrame *out);
 
 #ifdef __cplusplus
 }
